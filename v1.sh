892a6d7a8d353a9971f8decfd52baf012578b127
Evgeniy <johnnybrut@icloud.com>
2021-05-14 17:40:45 +0300

    meanings

--- v1.sh
+++ v1.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
-number1=10
+number1=35
 number2=10
 if [ $number1 -eq $number2 ]
 then
 echo "EQUAL"
 elif [ $number1 -gt $number2 ] 
 then
-echo "number1 is bigger than number2"
+echo "number1 is BIGGER than number2"
 elif [ $number1 -lt $number2 ] 
 then
-echo "number1 is smaller than number2"
+echo "number1 is SMALLER than number2"
 else
 echo "unknown number"
 fi
+# -eq это равенство
+# -ge больше или равно
+# -gt больше
+# -le меньше или равно
+# -lt меньше
+# -le  не равно
